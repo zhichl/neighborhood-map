48ef0c6d73ad6150362e71e3d8132e8c30c58782
zhichl <e.mbryo@hotmail.com>
2017-10-23 22:35:37 -0700

    Add part of location data

---
+++ js/models/locations.js
@@ -0,0 +1,52 @@
+const locations = [{
+	name: "Crab House Pier 39",
+	address: "203 Pier 39, San Francisco, CA 94133, USA",
+	lat: 37.809398, 
+	lng: -122.410355,
+	types: "Types: bar, establishment, food, point_of_interest, restaurant",
+	placeID: "ChIJ9T3A5PyAhYARce9_Mje3PoE"
+},
+
+{
+	name: "China Town",
+	address: "Chinatown, San Francisco, CA, USA",
+	lat: 37.794138,
+	lng: -122.407791,
+	types: "neighborhood, political",
+	placeID: "ChIJHYqlWIuAhYARrIAPZpIwnyg"
+},
+
+{
+	name: "Lombard Street",
+	address: "Lombard St, San Francisco, CA 94133, USA",
+	lat: 37.802139,
+	lng: -122.41874,
+	types: "establishment, point_of_interest",
+	placeID: "ChIJgQ-3a-aAhYARmRLswQrO0xA"
+},
+
+{
+	name: "Madame Tussauds San Francisco",
+},
+
+{
+	name: "San Francisco Museum of Modern",
+},
+
+{
+	name: "Asian Art Museum",
+},
+
+{
+	name: "San Francisco Art Institute",
+},
+
+{
+	name: "Golden Gate Bridge",
+},
+
+{
+	name: "Alcatraz Island",
+},
+
+]
