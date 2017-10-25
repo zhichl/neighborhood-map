8eb23d052cc75f3e296ce15e304b3118d6c40c8f
zhichl <e.mbryo@hotmail.com>
2017-10-24 18:32:57 -0700

    Wrap lat and lng in position object

--- js/models/locations.js
+++ js/models/locations.js
@@ -1,8 +1,10 @@
 const locations = [{
 	name: "Crab House Pier 39",
 	address: "203 Pier 39, San Francisco, CA 94133, USA",
-	lat: 37.809398, 
-	lng: -122.410355,
+	position: {
+		lat: 37.809398, 
+		lng: -122.410355
+	},
 	types: "Types: bar, establishment, food, point_of_interest, restaurant",
 	placeID: "ChIJ9T3A5PyAhYARce9_Mje3PoE"
 },
@@ -10,8 +12,10 @@ const locations = [{
 {
 	name: "China Town",
 	address: "Chinatown, San Francisco, CA, USA",
-	lat: 37.794138,
-	lng: -122.407791,
+	position: {
+		lat: 37.794138,
+		lng: -122.407791
+	},
 	types: "neighborhood, political",
 	placeID: "ChIJHYqlWIuAhYARrIAPZpIwnyg"
 },
@@ -19,8 +23,10 @@ const locations = [{
 {
 	name: "Lombard Street",
 	address: "Lombard St, San Francisco, CA 94133, USA",
-	lat: 37.802139,
-	lng: -122.41874,
+	position: {
+		lat: 37.802139,
+		lng: -122.41874
+	},
 	types: "establishment, point_of_interest",
 	placeID: "ChIJgQ-3a-aAhYARmRLswQrO0xA"
 },
@@ -28,8 +34,10 @@ const locations = [{
 {
 	name: "Madame Tussauds San Francisco",
 	address: "145 Jefferson St #600, San Francisco, CA 94133, USA",
-	lat: "37.8082263",
-	lng: "-122.41486",
+	position: {
+		lat: "37.8082263",
+		lng: "-122.41486"
+	},
 	types: "establishment, museum, point_of_interest, store, travel_agency",
 	placeID: "ChIJy3Ksp-OAhYARthoGyFztDtA", 
 },
@@ -37,8 +45,10 @@ const locations = [{
 {
 	name: "San Francisco Museum of Modern",
 	address: "151 3rd St, San Francisco, CA 94103, USA",
-	lat: "37.785718",
-	lng: "-122.401051",
+	position: {
+		lat: "37.785718",
+		lng: "-122.401051"
+	},
 	types: "art_gallery, establishment, museum, point_of_interest",
 	placeID: "ChIJ53I1Yn2AhYAR_Vl1vNygfMg" 
 },
@@ -46,8 +56,10 @@ const locations = [{
 {
 	name: "Asian Art Museum",
 	address: "200 Larkin St, San Francisco, CA 94102, USA",
-	lat: "37.780164",
-	lng: "-122.416198",
+	position: {	
+		lat: "37.780164",
+		lng: "-122.416198"
+	},
 	types: "establishment, museum, point_of_interest",
 	placeID: "ChIJkQQVTZqAhYARHxPt2iJkm1Q" 
 },
@@ -55,8 +67,10 @@ const locations = [{
 {
 	name: "San Francisco Art Institute",
 	address: "800 Chestnut St, San Francisco, CA 94133, USA",
-	lat: "37.803376",
-	lng: "-122.417167",
+	position: {
+		lat: "37.803376",
+		lng: "-122.417167"
+	},
 	types: "establishment, point_of_interest, school, university",
 	placeID: "ChIJ5WLIyuWAhYARbzm2ZWKavPY" 
 },
@@ -64,8 +78,10 @@ const locations = [{
 {
 	name: "Golden Gate Bridge",
 	address: "Golden Gate Bridge, San Francisco, CA, USA",
-	lat: "37.819929",
-	lng: "-122.478255",
+	position: {
+		lat: "37.819929",
+		lng: "-122.478255"
+	},
 	types: "establishment, point_of_interest",
 	placeID: "ChIJw____96GhYARCVVwg5cT7c0" 
 },
@@ -73,8 +89,10 @@ const locations = [{
 {
 	name: "Alcatraz Island",
 	address: "Alcatraz Island, San Francisco, CA 94133, USA",
-	lat: "37.826978",
-	lng: "-122.422955",
+	position: {
+		lat: "37.826978",
+		lng: "-122.422955"
+	},
 	types: "establishment, natural_feature, point_of_interest",
 	placeID: "ChIJmRyMs_mAhYARpViaf6JEWNE" 
 }
